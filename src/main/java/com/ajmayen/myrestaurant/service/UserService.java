58015dc259b9f3c989618254fb1a39cdb5bb2de6
Ajmayen Fayek <fayek22205341186@diu.edu.bd>
2025-03-18 15:05:51 +0600

    Register User

--- src/main/java/com/ajmayen/myrestaurant/service/UserService.java
+++ src/main/java/com/ajmayen/myrestaurant/service/UserService.java
@@ -1,4 +1,28 @@
 package com.ajmayen.myrestaurant.service;
 
+import com.ajmayen.myrestaurant.model.RestaurantUser;
+import com.ajmayen.myrestaurant.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public RestaurantUser registerUser(RestaurantUser restaurantUser) {
+
+        restaurantUser.setRestaurantName(restaurantUser.getRestaurantName());
+        restaurantUser.setRestaurantAddress(restaurantUser.getRestaurantAddress());
+        restaurantUser.setOwnerPhoneNumber(restaurantUser.getOwnerPhoneNumber());
+        restaurantUser.setRestaurantEmailAddress(restaurantUser.getRestaurantAddress());
+        restaurantUser.setPassword(restaurantUser.getPassword());
+        return userRepository.save(restaurantUser);
+       
+    }
 }
